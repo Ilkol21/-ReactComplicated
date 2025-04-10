73b00750462816054332caf8b875a04a3eb1aa59
illia <illya.kolom01@gmail.com>
2025-04-11 01:52:10 +0300

    change4

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,11 +4,36 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  // JS + TS базовые правила
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    extends: ["js/recommended"],
+  },
+
+  // TypeScript-специфические правила
+  {
+    ...tseslint.configs.recommended[0], // так подключаются правила typescript-eslint
+    files: ["**/*.{ts,tsx}"],
+  },
+
+  // React-правила
+  {
+    ...pluginReact.configs.flat.recommended,
+    files: ["**/*.{jsx,tsx}"],
+    settings: {
+      react: {
+        version: "detect", // Автоматически определяет версию React
+      },
+    },
+  },
 ]);
+
 
